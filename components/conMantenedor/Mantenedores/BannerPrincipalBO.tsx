12ba17f527d632cbb6b9d6009476d9186004946c
igcabrera.dev <hola.pixelup@gmail.com>
2024-06-21 05:19:24 -0400

    banner slider

--- components/conMantenedor/Mantenedores/BannerPrincipalBO.tsx
+++ components/conMantenedor/Mantenedores/BannerPrincipalBO.tsx
@@ -4,40 +4,58 @@ import React, { useState, useEffect, ChangeEvent } from "react";
 import axios from "axios";
 import { getCookie } from "cookies-next";
 
-const BannerPrincipalBO = () => {
-  const [bannerData, setBannerData] = useState<any | null>(null);
-  const [mainImage, setMainImage] = useState<string | null>(null);
-  const [formData, setFormData] = useState<any>({
+interface BannerImage {
+  id: string;
+  url: any;
+  title: string;
+  landingText: string;
+  buttonLink: string;
+  buttonText: string;
+  mainImageLink: string;
+  orderNumber: number;
+  mainImage: {
+    url: any;
+    name: any;
+    type: any;
+    size: any;
+    data: any;
+  };
+}
+
+const BannerPrincipalBO: React.FC = () => {
+  const [isMainImageUploaded, setIsMainImageUploaded] = useState(false);
+  const [isAddingImage, setIsAddingImage] = useState<boolean>(false);
+  const [isPreviewImageUploaded, setIsPreviewImageUploaded] = useState(false);
+  const [bannerData, setBannerData] = useState<BannerImage[]>([]);
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const [formData, setFormData] = useState<BannerImage>({
+    id: "",
+    url: "",
     title: "",
     landingText: "",
     buttonLink: "",
     buttonText: "",
-  });
-  const [loading, setLoading] = useState<boolean>(true);
-  const [updatedBannerData, setUpdatedBannerData] = useState({
-    title: "",
-    landingText: "",
-    buttonLink: "",
-    buttonText: "",
-    mainImageLink: "",
-    orderNumber: 1, // Modifica este valor según tu lógica
+    mainImageLink: "pixelup.cl",
+    orderNumber: 1,
     mainImage: {
+      url: "",
       name: "",
       type: "",
       size: null,
       data: "",
     },
   });
+  const [loading, setLoading] = useState<boolean>(true);
+  const [mainImage, setMainImage] = useState<string | null>(null);
 
   const fetchBannerHome = async () => {
     try {
-      setLoading(true); // Mostrar el indicador de carga
+      setLoading(true);
       const token = getCookie("AdminTokenAuth");
       const bannerId = "d24e992c-ac47-44f0-a9e6-fa5b6b8f7bfc";
-      const bannerImageId = "e38d6684-20c2-4a08-a46d-4a45306cacad";
 
-      const productTypeResponse = await axios.get(
-        `${process.env.NEXT_PUBLIC_API_URL_BO_CLIENTE}/api/v1/banners/${bannerId}/images/${bannerImageId}`,
+      const response = await axios.get(
+        `${process.env.NEXT_PUBLIC_API_URL_BO_CLIENTE}/api/v1/banners/${bannerId}/images`,
         {
           headers: {
             Authorization: `Bearer ${token}`,
@@ -46,79 +64,30 @@ const BannerPrincipalBO = () => {
         }
       );
 
-      const bannerImage = productTypeResponse.data.bannerImage;
-      setBannerData(bannerImage);
-      setFormData({
-        title: bannerImage.title,
-        landingText: bannerImage.landingText,
-        buttonLink: bannerImage.buttonLink,
-        buttonText: bannerImage.buttonText,
-        mainImageLink: bannerImage.mainImageLink,
-        orderNumber: 1,
-      });
+      console.log(response.data, "bannerData");
+      setBannerData(response.data.bannerImages);
+      if (response.data.bannerImages.length > 0) {
+        const initialImage = response.data.bannerImages[0];
+        setFormData(initialImage);
+        setMainImage(initialImage.mainImage.url || initialImage.mainImage.data);
+      }
     } catch (error) {
-      console.error("Error al obtener los tipos de producto:", error);
-      // Manejar el error según sea necesario
+      console.error("Error al obtener los datos del banner:", error);
     } finally {
-      setLoading(false); // Ocultar el indicador de carga
+      setLoading(false);
     }
   };
 
   useEffect(() => {
     fetchBannerHome();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []); // Debería ejecutarse solo en el montaje inicial
+  }, []);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    try {
-      setLoading(true); // Mostrar el indicador de carga
-      const token = getCookie("AdminTokenAuth");
-
-      // Crear un objeto de datos actualizado excluyendo mainImage si no hay una imagen seleccionada
-      const updatedDataWithoutImage = {
-        ...formData,
-        orderNumber: formData.orderNumber,
-      };
-
-      if (!mainImage) {
-        delete updatedDataWithoutImage.mainImage;
-      }
-
-      // Send updated data to the server
-      const bannerId = "d24e992c-ac47-44f0-a9e6-fa5b6b8f7bfc";
-      const bannerImageId = "e38d6684-20c2-4a08-a46d-4a45306cacad";
-      await axios.put(
-        `${process.env.NEXT_PUBLIC_API_URL_BO_CLIENTE}/api/v1/banners/${bannerId}/images/${bannerImageId}`,
-        updatedDataWithoutImage,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-            "Content-Type": "application/json",
-          },
-        }
-      );
-
-      // Optionally, you can refetch the banner data to ensure it's updated
-      fetchBannerHome();
-    } catch (error) {
-      console.error("Error updating banner:", error);
-      // Handle error
-    } finally {
-      setLoading(false); // Ocultar el indicador de carga
-    }
-  };
-
-  const handleImageChange = (
-    e: ChangeEvent<HTMLInputElement>,
-    setImage: React.Dispatch<React.SetStateAction<string | null>>,
-    imageKey: string
-  ) => {
+  const handleImageChange = (e: any, setImage: any, imageKey: any) => {
     const file = e.target.files?.[0];
     if (file) {
       const reader = new FileReader();
@@ -132,13 +101,14 @@ const BannerPrincipalBO = () => {
           size: file.size,
           data: result,
         };
-        setFormData((prevFormData: any) => ({
+        // Determina qué imagen se está cambiando y actualiza el estado correspondiente
+        if (imageKey === "mainImage") {
+          setIsMainImageUploaded(true);
+        } else if (imageKey === "previewImage") {
+          setIsPreviewImageUploaded(true);
+        }
+        setFormData((prevFormData) => ({
           ...prevFormData,
-          [imageKey]: imageInfo,
-        }));
-        // Actualizar updatedBannerData con la información de la imagen
-        setUpdatedBannerData((prevData: any) => ({
-          ...prevData,
           [imageKey]: imageInfo,
         }));
       };
@@ -146,10 +116,130 @@ const BannerPrincipalBO = () => {
     }
   };
 
-  const handleClearImage = (
-    setImage: React.Dispatch<React.SetStateAction<string | null>>
-  ) => {
-    setImage(null); // Limpiar la imagen seleccionada
+  const handleClearImage = () => {
+    setMainImage(null);
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const token = getCookie("AdminTokenAuth");
+      const bannerId = "d24e992c-ac47-44f0-a9e6-fa5b6b8f7bfc";
+
+      if (isAddingImage) {
+        // Realizar una solicitud POST para crear una nueva imagen
+        const newData = {
+          ...formData,
+          mainImageLink: "pixelup.cl",
+        };
+
+        await axios.post(
+          `${process.env.NEXT_PUBLIC_API_URL_BO_CLIENTE}/api/v1/banners/${bannerId}/images`,
+          newData,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
+        setIsAddingImage(false); // Resetear el estado después de agregar
+      } else {
+        // Realizar una solicitud PUT para actualizar la imagen existente
+        let updatedData = {
+          ...formData,
+          orderNumber: formData.orderNumber,
+        };
+
+        // Filtrar el campo mainImage si no se ha cargado una nueva imagen
+        if (!isMainImageUploaded) {
+          delete updatedData.mainImage;
+        }
+
+        await axios.put(
+          `${process.env.NEXT_PUBLIC_API_URL_BO_CLIENTE}/api/v1/banners/${bannerId}/images/${formData.id}`,
+          updatedData,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+              "Content-Type": "application/json",
+            },
+          }
+        );
+      }
+
+      fetchBannerHome();
+    } catch (error) {
+      console.error("Error al actualizar el banner:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleDeleteImage = async () => {
+    try {
+      setLoading(true);
+      const token = getCookie("AdminTokenAuth");
+
+      const bannerId = "d24e992c-ac47-44f0-a9e6-fa5b6b8f7bfc";
+      await axios.delete(
+        `${process.env.NEXT_PUBLIC_API_URL_BO_CLIENTE}/api/v1/banners/${bannerId}/images/${formData.id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      fetchBannerHome();
+    } catch (error) {
+      console.error("Error al borrar la imagen del banner:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleNextImage = () => {
+    const nextIndex = (currentIndex + 1) % bannerData.length;
+    setCurrentIndex(nextIndex);
+    const nextImage = bannerData[nextIndex];
+    setFormData(nextImage);
+    setMainImage(nextImage.mainImage.url || nextImage.mainImage.data);
+    setIsMainImageUploaded(false); // Reiniciar el estado de la imagen cargada
+  };
+
+  const handlePrevImage = () => {
+    const prevIndex =
+      (currentIndex - 1 + bannerData.length) % bannerData.length;
+    setCurrentIndex(prevIndex);
+    const prevImage = bannerData[prevIndex];
+    setFormData(prevImage);
+    setMainImage(prevImage.mainImage.url || prevImage.mainImage.data);
+    setIsMainImageUploaded(false); // Reiniciar el estado de la imagen cargada
+  };
+  const handleAddImageClick = () => {
+    setFormData({
+      id: "",
+      url: "",
+      title: "",
+      landingText: "",
+      buttonLink: "",
+      buttonText: "",
+      mainImageLink: "",
+      orderNumber: 1,
+      mainImage: {
+        url: "",
+        name: "",
+        type: "",
+        size: null,
+        data: "",
+      },
+    });
+    setMainImage(null);
+    setIsAddingImage(true);
   };
 
   if (loading) {
@@ -183,37 +273,61 @@ const BannerPrincipalBO = () => {
       id="banner"
       className="w-full"
     >
-      <div className="relative font-sans  before:absolute before:w-full before:h-full before:inset-0 before:bg-black before:opacity-30 before:z-10">
+      <div className="relative font-sans before:absolute before:w-full before:h-full before:inset-0 before:bg-black before:opacity-30 before:z-10">
         <img
-          src={bannerData.mainImage.url}
+          src={mainImage || formData.mainImage.url}
           alt="Banner Image"
           className="absolute inset-0 w-full h-full object-cover"
         />
         <div className="min-h-[300px] relative z-10 h-full max-w-6xl mx-auto flex flex-col justify-center items-center text-center text-white p-6">
-          <h2 className="text-2xl font-semibold mb-2">{bannerData.title}</h2>
+          <h2 className="text-2xl font-semibold mb-2">{formData.title}</h2>
           <p className="text-md text-center text-gray-200">
-            {bannerData.landingText}
+            {formData.landingText}
           </p>
           <a
-            href={bannerData.buttonLink}
-            className="mt-8 bg-dark bg-primary text-secondary hover:text-primary text-base font-semibold py-2.5 px-6  rounded hover:bg-secondary"
+            href={formData.buttonLink}
+            className="mt-8 bg-dark bg-primary text-secondary hover:text-primary text-base font-semibold py-2.5 px-6 rounded hover:bg-secondary"
           >
-            {bannerData.buttonText}
+            {formData.buttonText}
           </a>
         </div>
       </div>
-
+      <div className="flex justify-between mt-6">
+        <button
+          type="button"
+          onClick={handleAddImageClick}
+          className="shadow bg-green-500 hover:bg-green-700 w-full uppercase text-white font-bold py-2 px-4 rounded flex-wrap"
+          style={{ borderRadius: "var(--radius)" }}
+        >
+          Agregar Imagen
+        </button>
+        <button
+          type="button"
+          onClick={handleDeleteImage}
+          className="shadow bg-red-500 hover:bg-red-700 w-full uppercase text-white font-bold py-2 px-4 rounded flex-wrap ml-4"
+          style={{ borderRadius: "var(--radius)" }}
+        >
+          Borrar Imagen
+        </button>
+      </div>
+      <div className="flex justify-center items-center mt-8">
+        <button
+          onClick={handlePrevImage}
+          className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-l"
+        >
+          Prev
+        </button>
+        <button
+          onClick={handleNextImage}
+          className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-r"
+        >
+          Next
+        </button>
+      </div>
       <form
         onSubmit={handleSubmit}
         className="px-4 mx-auto mt-8"
       >
-        <input
-          type="number"
-          name="orderNumber"
-          value={formData.orderNumber}
-          onChange={handleChange}
-          className="hidden w-full px-4 py-2 mb-4 border border-gray-300 rounded-md"
-        />
         <h3 className="font-normal text-primary">
           Titulo <span className="text-primary">*</span>
         </h3>
@@ -225,13 +339,6 @@ const BannerPrincipalBO = () => {
           className="shadow block w-full px-4 py-3 mt-2 mb-4 border border-gray-300 rounded-md"
           placeholder="Title"
         />
-        <input
-          type="text"
-          name="mainImageLink"
-          value={formData.mainImageLink}
-          onChange={handleChange}
-          className="hidden w-full px-4 py-2 mb-4 border border-gray-300 rounded-md"
-        />{" "}
         <h3 className="font-normal text-primary">
           Texto <span className="text-primary">*</span>
         </h3>
@@ -290,7 +397,7 @@ const BannerPrincipalBO = () => {
                 />
                 <button
                   className="absolute top-0 right-0 bg-red-500 hover:bg-red-700 text-white rounded-full p-1 m-1 text-xs"
-                  onClick={() => handleClearImage(setMainImage)}
+                  onClick={handleClearImage}
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -343,10 +450,11 @@ const BannerPrincipalBO = () => {
             </div>
           )}
         </div>
+
         <button
           type="submit"
           disabled={loading}
-          className="shadow bg-primary hover:bg-secondary w-full uppercase text-secondary hover:text-primary  font-bold py-2 px-4 rounded flex-wrap mt-6"
+          className="shadow bg-primary hover:bg-secondary w-full uppercase text-secondary hover:text-primary font-bold py-2 px-4 rounded flex-wrap mt-6"
           style={{ borderRadius: "var(--radius)" }}
         >
           <svg
@@ -365,10 +473,14 @@ const BannerPrincipalBO = () => {
             />
             <path
               d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-              fill="currentColor"
+              fill="currentFill"
             />
           </svg>
-          {loading ? "Loading..." : "Actualizar"}
+          {loading
+            ? "Loading..."
+            : isAddingImage
+            ? "Crear Imagen"
+            : "Actualizar"}
         </button>
       </form>
     </section>
