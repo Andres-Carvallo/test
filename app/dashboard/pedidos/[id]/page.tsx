7bc95a4d341ee714ebc1fa8c2ec89ec776d5206b
pixelup-dev <hola.pixelup@gmail.com>
2025-02-06 16:41:57 -0300

    placeholder producto sin foto

--- app/dashboard/pedidos/[id]/page.tsx
+++ app/dashboard/pedidos/[id]/page.tsx
@@ -1,6 +1,13 @@
 "use client";
 /* eslint-disable @next/next/no-img-element */
-import { useEffect, useState, useRef } from "react";
+import {
+  useEffect,
+  useState,
+  useRef,
+  HTMLAttributes,
+  DetailedHTMLProps,
+  ImgHTMLAttributes,
+} from "react";
 import { obtenerOrdenesId } from "@/app/utils/obtenerOrdenesIDBO";
 import { getCookie } from "cookies-next";
 import { useParams } from "next/navigation";
@@ -11,6 +18,7 @@ import Link from "next/link";
 import Loader from "@/components/common/Loader";
 import toast from "react-hot-toast";
 import axios from "axios";
+
 interface Order {
   id: string;
   correlative: string;
@@ -71,6 +79,54 @@ interface Order {
     };
   }[];
 }
+
+interface ImageProps {
+  src: string | null | undefined;
+  alt: string;
+}
+
+const ImageWithFallback = ({ src = "", alt }: ImageProps) => {
+  const [error, setError] = useState(false);
+
+  const handleError = () => {
+    setError(true);
+  };
+
+  const imageClasses = "w-16 h-16 object-cover rounded-lg";
+  const fallbackClasses =
+    "w-16 h-16 flex items-center justify-center bg-gray-200 rounded-lg";
+
+  if (!src || error) {
+    return (
+      <div className={fallbackClasses}>
+        <svg
+          className="w-10 h-10 text-gray-400"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          aria-hidden="true"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
+          />
+        </svg>
+      </div>
+    );
+  }
+
+  return (
+    <img
+      src={src}
+      alt={alt}
+      className={imageClasses}
+      onError={handleError}
+    />
+  );
+};
 
 export default function DetalleOrdenes() {
   const { id } = useParams();
@@ -275,10 +331,9 @@ export default function DetalleOrdenes() {
                 >
                   {/* Imagen del producto */}
                   <div className="sm:w-24 w-full mb-4 sm:mb-0 sm:mr-4 flex justify-center">
-                    <img
+                    <ImageWithFallback
                       src={item.sku.mainImageUrl}
                       alt={item.sku.product.name}
-                      className="w-16 h-16 object-cover rounded-lg"
                     />
                   </div>
 
