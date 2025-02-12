c288cb680044507588c345c6d810dd9f7725af34
pixelup-dev <hola.pixelup@gmail.com>
2025-02-12 11:03:25 -0300

    fx

--- app/dashboard/productos/crear/producto-variable/VariationForm.tsx
+++ app/dashboard/productos/crear/producto-variable/VariationForm.tsx
@@ -16,6 +16,7 @@ import dynamic from "next/dynamic";
 import "react-quill/dist/quill.snow.css"; // Import styles
 import { useRevalidation } from "@/app/Context/RevalidationContext";
 import { slugify } from "@/app/utils/slugify";
+import LoaderProgress from "@/components/common/LoaderProgress";
 
 const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
 
@@ -682,11 +683,9 @@ const VariationForm: React.FC<any> = ({
     }
   };
 
-  if (isLoading) {
-    return <Loader />;
-  }
   return (
     <>
+      {isLoading && <LoaderProgress />}
       <div
         className="mt-4"
         ref={editFormRef}
@@ -975,16 +974,32 @@ const VariationForm: React.FC<any> = ({
         </div>
 
         <div className="grid grid-cols-4 mt-8 gap-4">
+          <div className="col-span-4 mb-4">
+            <button
+              onClick={handleCopyBaseImages}
+              className="bg-primary text-white px-4 py-2 rounded text-sm hover:bg-opacity-90 transition-colors flex items-center gap-2"
+              type="button"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-5 h-5"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M9 8.25H7.5a2.25 2.25 0 0 0-2.25 2.25v9a2.25 2.25 0 0 0 2.25 2.25h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25H15m0-3-3-3m0 0-3 3m3-3V15"
+                />
+              </svg>
+              Copiar imágenes del producto base
+            </button>
+          </div>
           <div className="col-span-4 md:col-span-1">
             <div className="flex justify-between items-center">
               <label className="font-normal">Imagen Principal</label>
-              <button
-                onClick={handleCopyBaseImages}
-                className="bg-primary text-white px-3 py-1 rounded text-sm hover:bg-opacity-90 transition-colors"
-                type="button"
-              >
-                Copiar imágenes base
-              </button>
             </div>
             <div className="mt-2 flex justify-center items-center">
               <ImageUpload
