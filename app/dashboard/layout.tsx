82027dda372d08c1862bd1f6506fc85a3c05af18
Gianfranco Marcolini Recalde <130308856+GMarcolini@users.noreply.github.com>
2025-02-13 17:39:11 -0300

    Fix scroll

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -77,7 +77,9 @@ export default function RootLayout({
             sidebarOpen={sidebarOpen}
             setSidebarOpen={setSidebarOpen}
           /> */}
+          <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
           <main className="mx-auto w-full p-4 md:p-6 2xl:p-10">{children}</main>
+          </div>
 
       </div>
     </div>
