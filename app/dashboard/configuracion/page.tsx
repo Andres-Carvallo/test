30cac97c9724774625ed0e8daffdb5fae1475d14
Gianfranco Marcolini Recalde <130308856+GMarcolini@users.noreply.github.com>
2025-05-15 11:39:56 -0400

    Cambios configuraciones

--- app/dashboard/configuracion/page.tsx
+++ app/dashboard/configuracion/page.tsx
@@ -25,7 +25,14 @@ const UsuariosPage = () => {
       <div className="flex flex-col mt-4 md:mt-10 gap-4 md:gap-10">
       <LogoEdit/>
       </div>
-
+      <div className="p-6 bg-white rounded-lg shadow-md border border-gray-200">
+          <div className="flex flex-col items-center justify-center text-center">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-12 h-12 text-blue-500 mb-4">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
+            <p className="text-gray-600 text-lg">¡Próximamente nuevas funcionalidades disponibles! Estamos trabajando para mejorar tu experiencia.</p>
+          </div>
+        </div>
 {/*       <div className="flex flex-col mt-4 md:mt-10 gap-4 md:gap-10 min-h-screen">
         <TableUsers />
       </div> */}
